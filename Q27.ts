5d94b6a06f495c3fc248abff0eafd8cff915f7db
mehwishshakeel <mehwishshakeel>
2024-03-27 00:26:51 +0500

    if and else statement

---
+++ Q27.ts
@@ -0,0 +1,34 @@
+let alian_Color1 = "red";
+// Using if and else statement
+// first version
+if(alian_Color1 === "green"){
+  console.log("player just earned 5 points for shooting the alien.");
+} 
+else if(alian_Color1 === "yellow"){
+  console.log("player just earned 10 points for shooting the alien.");
+}
+ else if(alian_Color1 === "red"){
+  console.log("player just earned 15 points for shooting the alien.");
+}
+// second version
+let alian_color2 = "yellow";
+if (alian_color2 === " green") {
+  console.log("player just earned 5 points for shooting the alien.");
+} 
+else if (alian_color2 === "yellow") {
+  console.log(" player just earned 10 points for shooting the alien.");
+} 
+else if (alian_color2 === "red") {
+  console.log("player just earned 15 points for shooting the alien.");
+}
+// Third Version
+let alian_color3="green"
+if(alian_color3 ="green"){ 
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else if(alian_color3 = "yellow"){
+    console.log("player just earned 10 points for shooting the alien.");
+}
+else if(alian_color3 ="red"){
+    console.log("player just earned 15 points for shooting the alien.");
+}
